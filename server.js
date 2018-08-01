17afc79c7a54445b0f42dfb868a70d835b2a5e14
sontis3 <oleg.ovcharenko@labworks.ru>
2018-08-01 17:55:09 +0300

    +mongoose

--- server.js
+++ server.js
@@ -3,12 +3,24 @@
 const Hapi = require('hapi');
 const HapiOpenAPI = require('hapi-openapi');
 const Path = require('path');
+const mongoose = require('mongoose');
+const mongoDbUri = 'mongodb://localhost:27017/lw_hapi_db';
 
 const init = async function () {
   const server = new Hapi.Server({
     host: 'localhost',
     port: 3000
   });
+
+  //connect with mongoDB
+  mongoose.connect(mongoDbUri, { useNewUrlParser: true });
+  mongoose.connection.on('connected', () => {
+    console.log(`app is connected to ${mongoDbUri}`);
+  });
+  mongoose.connection.on('error', err => {
+    console.log('error while connecting to mongodb', err);
+  });
+  //////////////////////////////////////////
 
   await server.register({
     plugin: HapiOpenAPI,
