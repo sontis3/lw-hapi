d1021d917128ed72c6e3ff9654f0c235fa12c7e5
sontis3 <oleg.ovcharenko@labworks.ru>
2018-10-02 18:38:47 +0300

    .

--- models/mongoose/catalogs/study.js
+++ models/mongoose/catalogs/study.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const studySchema = new Schema({
   code: { required: true, type: String, unique: true, uppercase: true, trim: true, minlength: 5, maxlength: 12 }, // код исследования
   customer: { type: Schema.Types.ObjectId, ref: 'Customer' },         // заказчик исследования
-  test_object: { type: Schema.Types.ObjectId, ref: 'Test_Object' },
+  test_object: { type: Schema.Types.ObjectId, ref: 'Test_Object' },   // тестируемые объекты
 
   date_created: { required: true, type: Date, default: Date.now },    // дата создания документа
   date_updated: { required: false, type: Date, default: Date.now },   // дата последнего изменения документа
