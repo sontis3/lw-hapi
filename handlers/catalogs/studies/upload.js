b1ee4f3c8a78793ee81dce31e28edb8a0ec2d5b9
sontis3 <oleg.ovcharenko@labworks.ru>
2018-12-14 19:20:12 +0300

    + upload stub

---
+++ handlers/catalogs/studies/upload.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const Boom = require('boom');
+
+/**
+ * Operations on /catalogs/studies/upload
+ */
+module.exports = {
+    /**
+     * summary: Загрузка файлов исследования
+     * description: 
+     * parameters: upFile, studyId
+     * produces: 
+     * responses: 200, 400
+     */
+    post: function uploadStudyFile(request, h) {
+        return Boom.notImplemented();
+    }
+};
